b3a39f50f42fd11cdcc976ef7e7707049d1e53ce
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2021-05-26 18:21:33 -0300

    Como visualizar algum item da tabela com com o SELECT

--- dio.sql
+++ dio.sql
@@ -1,3 +1,6 @@
+
+/*Criando a tabela*/
+
 CREATE TABLE pessoas (
     id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(30) NOT NULL, 
@@ -5,6 +8,18 @@ CREATE TABLE pessoas (
 
 )
 
+/*Inserindo dados na tabela*/
+
 INSERT INTO pessoas (nome, nascimento) VALUES ('Lethicia', '1999-02-10')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Nadia', '1996-10-25')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Pedro Lucas', '2015-04-20')
+
+
+/*'SELECT' é utilizado sempre que eu quero visualizar algo na tabela*/
+
+SELECT (nome)  FROM pessoa 
+
+ OU
+
+SELECT (nascimento)  FROM pessoa 
+
